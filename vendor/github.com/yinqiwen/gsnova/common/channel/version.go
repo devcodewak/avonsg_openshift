0e1b741fc86b5b733ee6d9a0c6f2558b24c28f5d
devcodewak <ubuntu7it@gmail.com>
2018-03-18 01:51:28 +0800

    d38d772

--- vendor/github.com/yinqiwen/gsnova/common/channel/version.go
+++ vendor/github.com/yinqiwen/gsnova/common/channel/version.go
@@ -3,4 +3,4 @@ package channel
 //'r1' means revision 1
 //const LocalVersion = "r15.1"
 //const RemoteVersion = "r12.1"
-const Version = "r16_v31_M23G1_0227" //"r16"
+const Version = "r16_v32_M23G2_0318" //"r16"
